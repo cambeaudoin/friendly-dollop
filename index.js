afb7f9522cd24809ab9eccf74a2acb7b0fa4424b
Cam <camilebeaudoin@gmail.com>
2017-02-01 21:06:54 -0500

    can scrape sites

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-var Xray = require('x-ray');
-var x = Xray();
-var fs = require('fs');
+const Xray = require('x-ray');
+const x = Xray();
+const fs = require('fs');
 
-// site you want to scrape
-var site = 'http://reddit.com';
-
-// selector to isolate
-var selector = '.content';
+// array of sites to test
+const resultsArray = [];
 
 // fs file to write to
-var fileDest = "results.txt";
+const fileDest = "results.txt";
 
-x(site, selector)(function (err, data) {
-    writeFile(data); // Google
-    console.log(data);
-})
 
-var writeFile = function (data) {
-    fs.writeFile(fileDest, data, function (err) {
+// selector to isolate
+const selector = x(
+    '.rankTable .wsTR', [{
+        site: ['td']
+    }]
+);
+
+const writeFile = function (data) {
+    fs.writeFile(fileDest, JSON.stringify(data), function (err) {
         if (err) {
             return console.log(err);
         }
-
         console.log("The file was saved!");
     });
 }
+const temp = function() {
+    for (var i = 1; i < 44; i++) {
+        // site you want to scrape
+        var site = 'https://domaintyper.com/top-websites/most-popular-websites-with-ca-domain/page/' + i;
+
+        x(site, selector)(function (err, data) {
+            data.forEach(function (p) {
+                console.log(p.site[1]);
+                resultsArray.push(p.site[i]);
+            })
+        })
+    }
+
+    writeFile(resultsArray);
+}
+
+temp();
